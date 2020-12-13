665a4f2e40a5eb114967f7f838e0b115fda9ff1e
Yunus Emre Tasova <emre.tasova@icloud.com>
2020-12-13 23:21:45 +0000

    Initial Commit

---
+++ gitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by Emre Tasova on 13/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
